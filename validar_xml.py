038a23991856ea59aa86a7bfd0ef9d9aa3167044
rohan241 <57097506+rohan241@users.noreply.github.com>
2020-02-17 13:01:50 +0100

    rohan

--- validar_xml.py
+++ validar_xml.py
@@ -1,6 +1,8 @@
-import xml.etree.ElementTree as ET
-archivo_xml = ET.parse('myBaraja.xml')
-raiz = archivo_xml.getroot()
-print(raiz)
-for hijo in raiz:
-    print(hijo.text)
+from xml.dom import minidom
+archivo = minidom.parse('mybaraja.xml')
+rai
+for playerconfig in
+playerlife= archivo.getElementsByTagName("playerLife")[0]
+print(playerlife.firstChild.data)
+summonpoints = archivo.getElementsByTagName("summonPointsPlayer")[0]
+print(summonpoints.sChild.data)
