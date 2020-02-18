541a43d5125781b8d1d0404e203a3a55c6a0b5b8
Arnau <minihirnau@hotmail.com>
2020-02-18 13:18:20 +0100

    kk

--- validar_xml.py
+++ validar_xml.py
@@ -1,11 +1,14 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('myBaraja.xml')
-root = tree.getroot()
-print(root)
-print(tree)
-for child in root:
-    print(child.tag,child.attrib)
-    for child2 in child:
-        print(child2.tag,child2.attrib)
-        for child3 in child2:
-            print(child3.tag,child3.attrib,child3.text)
+def cargar(nomobre):
+    if nombre == "aliado":
+        try:
+            fitxer2 = ET.parse("myBaraja.xml")
+            return fitxer2
+        except ValueError:
+            print("No se ha podido cargar el fichero")
+    else:
+        try:
+            fitxer1 = ET.parse("Enemigo.xml")
+            return fixter1
+        except ValueError:
+            print("no se ha podido cargar el fichero")
