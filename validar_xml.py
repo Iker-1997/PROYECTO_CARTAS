aaaa8e8dfb84bfeebafe0f6ba740c09fdc6e25cb
Arnau <minihirnau@hotmail.com>
2020-02-18 13:21:24 +0100

    kk

--- validar_xml.py
+++ validar_xml.py
@@ -1,14 +1,14 @@
 import xml.etree.ElementTree as ET
 def cargar(nomobre):
-    if nombre == "aliado":
+    if nombre == "aliat":
         try:
             fitxer2 = ET.parse("myBaraja.xml")
             return fitxer2
         except ValueError:
-            print("No se ha podido cargar el fichero")
+            print("No sha pogut carregar el fitxer")
     else:
         try:
             fitxer1 = ET.parse("Enemigo.xml")
             return fixter1
         except ValueError:
-            print("no se ha podido cargar el fichero")
+            print("No sha pogut carregar el fitxer")
