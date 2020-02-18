2293593e931da28a6699d11fe9921682fc059570
Arnau <minihirnau@hotmail.com>
2020-02-18 12:43:06 +0100

    ed

--- validar_xml.py
+++ validar_xml.py
@@ -2,9 +2,10 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('myBaraja.xml')
 root = tree.getroot()
 print(root)
+print(tree)
 for child in root:
     print(child.tag,child.attrib)
     for child2 in child:
         print(child2.tag,child2.attrib)
         for child3 in child2:
-            print(child3.tag,child3.attrib)
+            print(child3.tag,child3.attrib,child3.text)
