e93c1aeb3c87cb5cda80f309aa50ec213fd08e22
Arnau <minihirnau@hotmail.com>
2020-02-19 08:52:04 +0100

    kk

--- validar_xml.py
+++ validar_xml.py
@@ -6,6 +6,7 @@ def cargar(nombre):
             return fitxer2
         except ValueError:
             print("No se ha podido leer el fichero")
+    # La otra opcion...
     else:
             try:
                 fitxer1 = ET.parse("Enemigo.xml")
