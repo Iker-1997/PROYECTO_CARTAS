ff6ba8ae01348d7509ae8bb4872aeb32f36b77b1
rohan241 <57097506+rohan241@users.noreply.github.com>
2020-03-03 12:44:26 +0100

    rohan

--- mazos.py
+++ mazos.py
@@ -6,14 +6,16 @@ def crearMazo(tipo, archivo):
 
     for i in range(10):
         x = str(random.randint(1,10))
+        llista_general = []
         for child in archivo.findall('.//card['+x+']'):
-
             for child2 in child:
                 print(child2.tag, child2.text, child2.attrib)
-
+                llista_general.append(child)
 
 
 import xml.etree.ElementTree as ET
 archivoa = ET.parse('./myBaraja.xml')
 archivoa = archivoa.getroot()
 crearMazo('attack', archivoa)
+import random
+
