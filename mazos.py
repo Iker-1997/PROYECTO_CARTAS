0e09f21308d70da71166207ea8a942a51c86b65c
Arnau <minihirnau@hotmail.com>
2020-03-03 11:53:13 +0100

    xpath random1

--- mazos.py
+++ mazos.py
@@ -1,18 +1,16 @@
+import random
 def crearMazo(tipo, archivo):
     types = ['attack', 'defend', 'random', 'balanced']
     # Mostramos un error si el atributo <tipo> no es el deseado con la lista creada en la anterior linea.
     assert tipo in types, 'El atributo insertado no es el correcto.'
 
-    if tipo is 'attack':
-        for child in archivo.findall('deck//'):
-            if child.tag == "attack" and child.text == "3":
-                print(child.tag, child.text)
-    elif tipo is 'defend':
-        print('Defensa:', archivo)
-    elif tipo is 'random':
-        print('Random:', archivo)
-    elif tipo is 'balanced':
-        print('Balanced:', archivo)
+    for i in range(10):
+        x = str(random.randint(1,10))
+        for child in archivo.findall('.//card['+x+']'):
+
+            for child2 in child:
+                print(child2.tag, child2.text, child2.attrib)
+
 
 
 import xml.etree.ElementTree as ET
