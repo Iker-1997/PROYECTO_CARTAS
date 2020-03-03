3c1bd3e2ae2521a8667082bb94c9d9f699dff4dd
rohan241 <57097506+rohan241@users.noreply.github.com>
2020-03-03 14:26:39 +0100

    Merge branch 'master' of C:\Users\rohan\PROYECTO_CARTAS with conflicts.

--- mazos.py
+++ mazos.py
@@ -1,22 +1,21 @@
 import random
-import xml.etree.ElementTree as ET
-archivoa = ET.parse('./myBaraja.xml')
-archivoa = archivoa.getroot()
 def crearMazo(archivo):
-    llista_general = []
+    deck = []
     card = {}
-    i=0
-    while i<10:
+    for i in range(10):
         x = str(random.randint(1,10))
         for child in archivo.findall('.//card['+x+']'):
             for child2 in child:
                 card[child2.tag] = child2.text
-            if card in llista_general:
-                pass
-            else:
-                i=i+1
-                llista_general.append(card)
-            card = {}
-    print(len(llista_general))
-    return llista_general
-print(crearMazo(archivoa))
+        deck.append(card)
+        print(deck)
+
+
+
+import xml.etree.ElementTree as ET
+archivoa = ET.parse('./myBaraja.xml')
+archivoa = archivoa.getroot()
+crearMazo(archivoa)
+
+
+r
