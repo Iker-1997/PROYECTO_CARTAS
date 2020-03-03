16c1574266e3c8d08f01b032381348a0cd46e66a
Arnau <minihirnau@hotmail.com>
2020-03-03 14:04:03 +0100

    xpath random1

--- mazos.py
+++ mazos.py
@@ -3,16 +3,20 @@ import xml.etree.ElementTree as ET
 archivoa = ET.parse('./myBaraja.xml')
 archivoa = archivoa.getroot()
 def crearMazo(archivo):
-    deck = []
+    llista_general = []
     card = {}
-
-    for i in range(10):
+    i=0
+    while i<10:
         x = str(random.randint(1,10))
         for child in archivo.findall('.//card['+x+']'):
             for child2 in child:
                 card[child2.tag] = child2.text
-        deck.append(card)
-        card = {}
-
-    return deck
+            if card in llista_general:
+                pass
+            else:
+                i=i+1
+                llista_general.append(card)
+            card = {}
+    print(len(llista_general))
+    return llista_general
 print(crearMazo(archivoa))
